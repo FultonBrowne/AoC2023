3f57bc6ed457f08722e5f352b2658921ff92da7a
Fulton Browne <fultonbrowne@pm.me>
2023-12-01 14:38:55 -0600

    And there it is

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,7 +1,20 @@
 use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
 
 fn main() {
     let mut input = "";
+    let mut o = 0;
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
@@ -9,27 +22,26 @@ fn main() {
     } else {
         return
     }
-
-    let mut y = None;
-    let mut z = None;
-    for i in input.chars() {
-        if i.is_digit(10){
-            y = Some(i);
-            break;
+    for l in read_lines(input).unwrap(){
+        let mut y = 0;
+        let mut z = 0;
+        let e = l.unwrap();
+        for i in e.chars() {
+            if i.is_digit(10){
+                y = i.to_digit(10).unwrap();
+                break;
+            }
+            
         }
-        
-    }
 
-    for i in input.chars().rev() {
-        if i.is_digit(10){
-            z = Some(i);
-            break;
+        for i in e.chars().rev() {
+            if i.is_digit(10){
+                z = i.to_digit(10).unwrap();
+                break;
+            }
         }
+        o += ((y * 10) + z)
     }
 
-    match (y, z) {
-        (Some(a), Some(b)) => println!("{}{}", a, b),
-        _ => println!("No digits found in the argument."),
-    }
-
+    println!("{}", o);
 }
